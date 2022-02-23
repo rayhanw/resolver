caa02fe5e48dcb5d77370d617c7dc94d137d9493
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-02-23 14:49:12 +0800

    add new and show page for error

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,25 @@
+class ErrorsController < ApplicationController
+  def show
+    @error = Error.find(params[:id])
+  end
+
+  def new
+    @error = Error.new
+  end
+
+  def create
+    @error = Error.new(error_params)
+
+    if @error.save
+      redirect_to error_path(@error)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def error_params
+    params.require(:error).permit(:title, :details, :solution)
+  end
+end
