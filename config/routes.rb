248bd03cee30ba1573881932d2adb49a026d11f2
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-02-23 12:39:01 +0800

    remove default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'errors/new'
-  get 'errors/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
 
