732cb0c405dff2182238a0388ae40ff523d5367b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-02 19:58:29 +0800

    use jit mode for tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,29 +1,35 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
-        code: ["Fira Code", "Roboto", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        black: "rgba(42, 42, 42, 1)",
-        gray: {
-          custom: "#3C3C3C",
-          light: "#F1FAEE",
-        },
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+	mode: "jit",
+	content: [
+		"./app/helpers/**/*.rb",
+		"./app/javascript/**/*.js",
+		"./app/views/**/*"
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+				code: ["Fira Code", "Roboto", ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				black: "rgba(42, 42, 42, 1)",
+				gray: {
+					custom: "#3C3C3C",
+					light: "#F1FAEE"
+				}
+			}
+		}
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/aspect-ratio"),
+		require("@tailwindcss/typography")
+	],
+	purge: [
+		"./app/**/*.html.erb",
+		"./app/helpers/**/*.rb",
+		"./app/javascript/**/*.js"
+	]
 };
