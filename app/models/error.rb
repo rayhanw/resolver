ac219e247823a31e05e6018aa14f112e6fb880ba
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-02 21:10:14 +0800

    add Error#upvote!

--- app/models/error.rb
+++ app/models/error.rb
@@ -6,4 +6,8 @@ class Error < ApplicationRecord
   def solution_converted
     @solution_converted ||= pipeline.call(solution.to_plain_text)[:output].to_s
   end
+
+  def upvote!
+    self.votes += 1
+  end
 end
