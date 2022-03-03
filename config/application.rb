d5a7e45df43ae62f142c0180031bda0ef8f5a01a
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-03 14:56:42 +0800

    download font locally

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module Resolver
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
