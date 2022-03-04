1d8f2415cb7365bc61b86c08774b50f1157dc223
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-04 16:37:40 +0800

    attempt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+file = YAML.load_file(Rails.root.join("db", "data", "errors.yml"))
+
+p file
