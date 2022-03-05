75030b950da0f0426ef32304aaa3d354dd9ec9f1
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-05 16:03:12 +0800

    add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 entries = YAML.load_file(Rails.root.join("db", "data", "errors.yml")).map { |ele| ele.transform_keys(&:to_sym) }
 
+User.destroy_all
 Error.destroy_all
+
 puts "Removing all errors"
+
+User.create!(
+  email: "admin@resolver.site",
+  password: ")T*fOE7KNKBL7FwsqN@",
+  admin: true
+)
+
+puts "Admin created"
 
 entries.each do |entry|
   error = Error.create!(entry)
