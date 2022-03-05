0255d80512cd9e141d376e6220d4e00cfc30de30
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-05 15:22:45 +0800

    add footer

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -2,7 +2,8 @@ module GithubHelper
   def gh_links
     {
       error: "https://github.com/rayhanw/resolver/issues/new?assignees=rayhanw&labels=enhancement&template=resolver-request.md&title=",
-      bug: "https://github.com/rayhanw/resolver/issues/new?assignees=rayhanw&labels=bug&template=bug_report.md&title="
+      bug: "https://github.com/rayhanw/resolver/issues/new?assignees=rayhanw&labels=bug&template=bug_report.md&title=",
+      codebase: "https://github.com/rayhanw/resolver"
     }
   end
 end
