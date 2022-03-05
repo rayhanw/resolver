f679d9100c9ffe29625a73b96533ffa6b966e5db
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-05 15:55:38 +0800

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-file = YAML.load_file(Rails.root.join("db", "data", "errors.yml"))
+entries = YAML.load_file(Rails.root.join("db", "data", "errors.yml")).map { |ele| ele.transform_keys(&:to_sym) }
 
-p file
+Error.destroy_all
+puts "Removing all errors"
+
+entries.each do |entry|
+  error = Error.create!(entry)
+
+  puts "Error ##{error.id} created!"
+end
