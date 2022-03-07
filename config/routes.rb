617a10c24049a181ab264228cdc2e8bf9b7e3ef4
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-07 16:05:45 +0800

    separate error show page for docs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :errors, only: %i[index show]
+      get "/error", to: "errors#show_api", as: :show_api
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
