29bac94e700106582158e12b212ea31e20838b50
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-07 16:55:10 +0800

    separate api syntax highlighter controller

---
+++ app/javascript/controllers/api_syntax_highlighter_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+import MarkdownIt from "markdown-it";
+import hljs from "highlight.js";
+
+export default class extends Controller {
+  static values = {
+    content: String,
+  };
+
+  connect() {
+    this.md = new MarkdownIt();
+    const content = this.contentValue;
+    const result = this.md.render(content);
+    this.element.innerHTML = result;
+    hljs.configure({
+      languages: ["json"],
+    });
+    hljs.highlightAll();
+  }
+}
