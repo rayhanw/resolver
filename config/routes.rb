f14760551d4e0dfc75f28f28e3fc1cc430db860a
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-07 15:29:21 +0800

    update route for only json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api, { format: :json } do
+    namespace :v1 do
+      resources :errors, only: %i[index show]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, skip: :registrations
   root "pages#home"
