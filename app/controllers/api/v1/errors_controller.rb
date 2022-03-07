1c1a102616ca9d8158ccc43f4436f297154aa882
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-07 16:46:17 +0800

    fix indentation for index content

--- app/controllers/api/v1/errors_controller.rb
+++ app/controllers/api/v1/errors_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::ErrorsController < Api::V1::BaseController
   def index
     @errors = Error.by_most_popular
+    @content = '[
+  &nbsp;&nbsp;{
+    &nbsp;&nbsp;&nbsp;&nbsp;"id": 1,
+    &nbsp;&nbsp;&nbsp;&nbsp;"title": "undefined method nil:NilClass",
+    &nbsp;&nbsp;&nbsp;&nbsp;"details": "Happens on ...",
+    &nbsp;&nbsp;&nbsp;&nbsp;"resolver": "Solution goes here"
+  &nbsp;&nbsp;},
+  &nbsp;&nbsp;{
+    &nbsp;&nbsp;&nbsp;&nbsp;"id": 2,
+    &nbsp;&nbsp;&nbsp;&nbsp;"title": "Heroku ...",
+    &nbsp;&nbsp;&nbsp;&nbsp;"details": "Happens on ...",
+    &nbsp;&nbsp;&nbsp;&nbsp;"resolver": "Solution goes here"
+  &nbsp;&nbsp;}
+]'
   end
 
   def show
