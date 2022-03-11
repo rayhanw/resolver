0485ebb8cd611b9d9958298ca8a88b480295b933
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-11 16:18:57 +0800

    hardcode default host for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = ENV["DOMAIN"]
+SitemapGenerator::Sitemap.default_host = ENV["DOMAIN"] || "https://resolver.site"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
