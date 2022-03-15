9eb67cc80c7152f703679aa13e353d0bbeb50a6c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-15 17:23:18 +0800

    tailwind custom bg position

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,30 +1,33 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	mode: "jit",
-	content: [
-		"./app/helpers/**/*.rb",
-		"./app/javascript/**/*.js",
-		"./app/views/**/*"
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Roboto", ...defaultTheme.fontFamily.sans],
-				code: ["Fira Code", "Roboto", ...defaultTheme.fontFamily.sans]
-			},
-			colors: {
-				black: "rgba(42, 42, 42, 1)",
-				gray: {
-					custom: "#3C3C3C",
-					light: "#F1FAEE"
-				}
-			}
-		}
-	},
-	plugins: [
-		require("@tailwindcss/forms"),
-		require("@tailwindcss/aspect-ratio"),
-		require("@tailwindcss/typography")
-	]
+  mode: "jit",
+  content: [
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        code: ["Fira Code", "Roboto", ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        black: "rgba(42, 42, 42, 1)",
+        gray: {
+          custom: "#3C3C3C",
+          light: "#F1FAEE"
+        }
+      },
+      backgroundPosition: {
+        "center-custom": "12px center"
+      }
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography")
+  ]
 };
