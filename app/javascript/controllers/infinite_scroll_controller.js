6295ce37fc33affc544333d4021562928862bfd3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-03-15 17:25:05 +0800

    infinite scroll

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -1,26 +1,52 @@
 import { Controller } from "@hotwired/stimulus";
+import { useIntersection } from "stimulus-use";
 
 export default class extends Controller {
-	static targets = ["list"];
-	static values = {
-		offset: Number
-	};
+  static targets = ["list", "trigger", "card"];
+  static values = {
+    offset: Number,
+    lastErrorId: Number,
+    initialText: String,
+    loadingText: String,
+    noMoreText: String
+  };
+  static classes = ["disabled"];
 
-	connect() {
-		console.log("connected");
-	}
+  connect() {
+    this.offset = 3;
+    this.hasMore = true;
+    useIntersection(this, { element: this.triggerTarget });
+  }
 
-	loadMore() {
-		const options = {
-			headers: {
-				Accept: "text/plain"
-			}
-		};
-		const offset = this.offsetValue + 3;
-		fetch(`/?offset=${offset}`, options)
-			.then(response => response.text())
-			.then(data => {
-				this.listTarget.insertAdjacentHTML("beforeend", data);
-			});
-	}
+  appear() {
+    if (this.hasMore) {
+      this.#loadMore();
+    }
+  }
+
+  #loadMore() {
+    this.triggerTarget.innerText = this.loadingTextValue;
+    const options = {
+      headers: {
+        Accept: "text/plain"
+      }
+    };
+    fetch(`/?offset=${this.offset}`, options)
+      .then((response) => response.text())
+      .then((data) => {
+        this.listTarget.insertAdjacentHTML("beforeend", data);
+        this.offset += 3;
+
+        const pattern = new RegExp(`error-${this.lastErrorIdValue}`);
+        this.hasMore = !pattern.test(data);
+
+        if (this.hasMore) {
+          this.triggerTarget.innerText = this.initialTextValue;
+        } else {
+          this.triggerTarget.setAttribute("disabled", "");
+          this.triggerTarget.innerText = this.noMoreTextValue;
+          this.triggerTarget.classList.add(...this.disabledClasses);
+        }
+      });
+  }
 }
