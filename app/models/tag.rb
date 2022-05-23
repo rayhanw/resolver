09e16b288b315efad9f71c941ed7e2a9de4f7121
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-05-23 11:57:16 +0800

    association

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,9 @@
 class Tag < ApplicationRecord
   validates :name, uniqueness: true
 
+  has_many :keywords
+  has_many :resolvers, through: :keywords, source: :error
+
   before_validation :capitalize_name
 
   private
